91a6c7e1de44cd4aa0c944e14a74ccd3730409ea
Affan Najiy <affannajiy@gmail.com>
2023-10-31 16:02:10 +0800

    5th commit

--- python/Lols.py
+++ python/Lols.py
@@ -5,5 +5,5 @@ name = input(str("What is your name: "))
 print("Your name is ",name)
 
 #Age
-age = input(int("What is your age: "))
-print("Your age is ",age)
+age = input(str("What is your age: "))
+print("Your age is ",age," year(s) old")
