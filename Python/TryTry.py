e785c1bf05959e1155d47a11ef30ca0fa4e96fb3
Affan Najiy <affannajiy@gmail.com>
2023-11-13 16:07:01 +0800

    日本語１

--- Python/TryTry.py
+++ Python/TryTry.py
@@ -2,17 +2,17 @@ print("Hello There!")
 print("This is a code to get to know more about you")
 print("--------------------")
 
-#name
+#name　名前
 name = input(str("What is your name? "))
 print("Hi",name,"nice to meet you.")
 print("--------------------")
 
-#age
+#age　年齢ですか。
 age = input(str("How old are you? "))
 print("You are",age,"year(s) old!")
 print
 
-#food
+#food　好きな食べ物
 food = input(str("What is your favourite food? "))
 print("Oh!",food,"?! That's my favourite too!")
 
@@ -20,6 +20,13 @@ print("Oh!",food,"?! That's my favourite too!")
 hobby = input(str("What is your hobby? "))
 print("Oh!",hobby,"?! That's my hobby too!")
 
-#from?
+#from?どこ？
 place = input(str("Where are you from? "))
 print("Wow!",place,"I'm from there too!")
+
+#日本語わかりましたか
+japanese = input(str("日本語はわかりましたか。")).lower()
+if japanese == "yes":
+  print("うわーすごいね！")
+elif japanese == "no":
+  print("あ、だいじょぶです！")
