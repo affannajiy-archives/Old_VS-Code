69ae8893772a4b11d0ee775e0e7c5d197cfed1ab
Affan Najiy <affannajiy@gmail.com>
2024-02-16 11:10:25 +0800

    labw6.2

--- Python/LabW6.py
+++ Python/LabW6.py
@@ -1,5 +1,5 @@
 import math
-
+'''
 #a)
 d_n = 1 / (1 + r)**n
 #b)
@@ -7,4 +7,17 @@ t_prime = t * (math.sqrt(1-(V**2 / c**2)))
 #c)
 V = 4/3 * pi * r**3
 #d)
-x = -b + (math.sqrt(b**2 - 4 * a * c)) / 2 * and
+x = -b + (math.sqrt(b**2 - 4 * a * c)) / 2 * a
+'''
+x = 2.5
+y = -1.5
+m = 18
+n = 4
+s = "Life"
+t = "Honey"
+print(x + n * y - (x + n) * y)
+print(m / n + m % n)
+print(5 * x - n / 5)
+print(math.sqrt (math. sqrt(n)))
+print(s + t)
+print(s + 'is sweet as'+ ' ' + t)
