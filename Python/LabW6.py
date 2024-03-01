2bc2221b923824173d6464ff5fa0a6d07a077bf1
Affan Najiy <affannajiy@gmail.com>
2024-03-01 10:37:01 +0800

    LabSAP

--- Python/LabW6.py
+++ Python/LabW6.py
@@ -1,5 +1,5 @@
 import math
-
+'''
 #a)
 d_n = 1 / (1 + r)**n
 #b)
@@ -8,7 +8,7 @@ t_prime = t * (math.sqrt(1-(V**2 / c**2)))
 V = 4/3 * pi * r**3
 #d)
 x = -b + (math.sqrt(b**2 - 4 * a * c)) / 2 * a
-
+'''
 
 x = 2.5
 y = -1.5
