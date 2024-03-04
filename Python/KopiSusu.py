60458cde7fcb5d3c4f05b863d25e2f3b04efe1ea
Affan Najiy <affannajiy@gmail.com>
2024-03-04 10:26:30 +0800

    LectureSAP

---
+++ Python/KopiSusu.py
@@ -0,0 +1,9 @@
+hehe = open("kopisusu.txt", "w")
+
+hehe.write("Senarai Kopi di Cafe Kopi Susu\n")
+hehe.write("Kopi O\n")
+hehe.write("Kopi Tarik\n")
+hehe.write("Kopi Huhui\n")
+hehe.write("Kopi Gula")
+
+hehe.close()
