82bf957dd4f7ddd5ffd09be76239c9213b98e16b
Affan Najiy <affannajiy@gmail.com>
2024-03-09 19:22:55 +0800

    LapSAPW9

--- Python/KopiSusu.py
+++ Python/KopiSusu.py
@@ -1,9 +1,24 @@
-hehe = open("kopisusu.txt", "w")
 
-hehe.write("Senarai Kopi di Cafe Kopi Susu\n")
+hehe = open("kopisusu.txt", "w") #writing the textfile
+
 hehe.write("Kopi O\n")
 hehe.write("Kopi Tarik\n")
-hehe.write("Kopi Huhui\n")
+hehe.write("Kopi Susu Tambah\n")
+hehe.write("Kopi Milo Ais\n")
 hehe.write("Kopi Gula")
 
 hehe.close()
+
+'''
+k = open("kopisusu.txt", "r")
+list1 = k.readlines()
+print(list1[3])
+
+'''
+'''
+print(k.readline(2)) #same as read(2)
+print(k.readline())
+
+print(k.read(7)) #it will continue after the 7 char including the space
+print(k.read(8))
+'''
