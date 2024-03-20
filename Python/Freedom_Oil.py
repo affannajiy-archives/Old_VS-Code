73a35327e05cca0856b833d56c9f6068a116955f
Affan Najiy <affannajiy@gmail.com>
2024-03-20 13:31:40 +0800

    Revamp

--- Python/Freedom_Oil.py
+++ Python/Freedom_Oil.py
@@ -1,19 +1,31 @@
-def refinery_system(crude_oil_list):
-  percent_crude_oil = []
-  total_crude_oil = sum(crude_oil_list)
-
-  for i in crude_oil_list:
-    percent_crude_oil.append(i / total_crude_oil * 100)
-  
-  gasoline = 0.45 * total_crude_oil
-  diesel = 0.35 * total_crude_oil
-  jet_fuel = 0.2 * total_crude_oil
-
-  return total_crude_oil, percent_crude_oil, gasoline, diesel, jet_fuel
-
-num = int(input("Please enter total number of refinery units: "))
 crude_oil_list = []
 
+def refinery_system(crude_oil_list):
+    total_crude_oil = sum(crude_oil_list) #sum of oil
+    percent_crude_oil = []
+
+    for i in crude_oil_list: #calc the percentage
+        percent_crude_oil.append(i / total_crude_oil * 100)
+
+    gasoline = 0.45 * total_crude_oil
+    diesel = 0.35 * total_crude_oil
+    jet_fuel = 0.20 * total_crude_oil
+
+    return total_crude_oil, percent_crude_oil, gasoline, diesel, jet_fuel
+
+num = int(input("Please enter total number of refinery units: "))
+
 for j in range(num):
-  unit = eval(input("Please enter the amount of crude oil in unit " + str(i+1) +":"))
-  crude_oil_list.append(unit)
+    unit = eval(input("Please enter the amount of crude oil in unit " + str(j + 1) + ": "))
+    crude_oil_list.append(unit)
+
+total_crude_oil, percent_crude_oil, gasoline, diesel, jet_fuel = refinery_system(crude_oil_list)
+
+print("\nTotal crude oil processed:", total_crude_oil)
+
+for x in range(num):
+    print("Percent of crude oil processed by unit", str(x + 1), ": ", round(percent_crude_oil[x], 2), "%")
+
+print("Gasoline Produced:", round(gasoline, 2))
+print("Diesel Produced:", round(diesel, 2))
+print("Jet Fuel Produced:", round(jet_fuel, 2))
--- Python/LabW8.py
+++ Python/LabW8.py
@@ -1,3 +1,80 @@
+#Lab Week 8
+#Activity 2
+#1
+#A
+if 12 < 12:
+  print('Less')
+else:
+  print('Not Less')
+
+#B
+Var1,Var2 = 25.12,15.00
+if Var1 <= Var2:
+  print('Less or equal')
+else:
+  print ('Greater than')
+
+#2
+#A
+Y,X = 15.0,25.0
+if Y != (X - 10.0):
+  X = X - 10
+else:
+  X = X / 2.0
+
+#B
+Y = 10.0
+X = 25.0
+if Y != (X - 10.0):
+  X = X - 10
+else:
+  X = X / 2.0
+
+#C
+Y = 15.0
+if (Y < 15.0) and (Y >= 0.0):
+  X = 5 * Y
+else:
+  X = 2 * Y
+
+#D
+Y = 10.0
+if (Y < 15.0) and (Y >= 0.0):
+  X = 5 * Y
+else:
+  X = 2 * Y
+
+#E
+Y = 36.0
+if (Y < 15.0) and (Y >= 0.0):
+  X = 5 * Y
+elif Y >20 :
+  X = 4 * Y
+else:
+  X = 2 * Y
+
+#F
+Y = -5
+if (Y < 15.0) and (Y >= 0.0):
+  X = 5 * Y
+elif Y >20:
+  X = 4 * Y
+else:
+  X = 2 * Y
+
+#G
+Y = 67
+if (Y < 15.0) and (Y >= 0.0):
+  X = 5 * Y
+elif Y >20:
+  if Y<30:
+    X = 4 * Y
+  else:
+    X = 0 * Y
+else:
+  X = 2 * Y
+
+#Activity 3
 #Teenager Internet Addiction
 
 print("!!Teenager Internet Addiction!!")
