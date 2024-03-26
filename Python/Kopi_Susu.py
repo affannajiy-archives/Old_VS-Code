9474448efa1ff18299cf9ef9470c76c2f0800312
Affan Najiy <affannajiy@gmail.com>
2024-03-26 13:49:11 +0800

    Kopi

--- Python/Kopi_Susu.py
+++ Python/Kopi_Susu.py
@@ -1,6 +1,10 @@
 
 kopi = open(r"C:\Users\user\OneDrive\Desktop\Visual Studio Code\Python\kopi_susu.txt", "w") #writing the textfile
 
+kopi.write("Koleksi Kopi-Kopi Tempatan\n")
+leng = "Koleksi Kopi-Kopi Tempatan"
+kopi.write("=" * len(leng))
+
 kopi.write("Kopi O\n")
 kopi.write("Kopi Tarik\n")
 kopi.write("Kopi Susu Tambah\n")
