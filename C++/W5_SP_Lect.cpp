9c0f5ce86fad26122022009ecb5bcd108db8077f
Affan Najiy <affannajiy@gmail.com>
2024-06-04 17:25:23 +0800

    W5 CPP Revamp 3

--- C++/W5_SP_Lect.cpp
+++ C++/W5_SP_Lect.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+/*#include <iostream>
 using namespace std;
 
 int main(){
@@ -12,5 +12,42 @@ int main(){
     case 404: cout << "Not Found"; break;
     case 500: cout << "Server Error"; break;
   }
+  return 0;
+}*/
+
+/*#include <iostream>
+using namespace std;
+
+int main(){
+  int number = 1;
+  while (number <= 5){
+  cout << "Hello\n";
+  }
+  return 0;
+}*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int score1, score2, score3;
+  double average;
+  char again;
+
+  do
+  {
+    // Get 3 Scores
+    cout << "Enter 3 scores and I will Average them: ";
+    cin >> score1 >> score2 >> score3;
+
+    //Calc & Display Avg
+    average = (score1 + score2 + score3) / 3.0;
+    cout << "The average is " << average << ".\n";
+
+    //Does user want again?
+    cout << "Do you want to average another set? (Y/N) ";
+    cin >> again;
+  } while (again == 'Y' || again == 'y'); 
   return 0;
 }
