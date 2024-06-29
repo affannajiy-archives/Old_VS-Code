77ededdf6597b8ee3a85f009a7032fff7ef976b3
Affan Najiy <affannajiy@gmail.com>
2024-06-29 14:06:11 +0800

    W8 Lab Revamp

--- C++/week8_activity6.cpp
+++ C++/week8_activity6.cpp
@@ -6,10 +6,22 @@ int main(){
   float cgpa[3];
 
   for (int i = 0; i < 3; i++){
-    for (int j = 0; j < 3; i++){
+    for (int j = 0; j < 3; j++){
       cout << "Enter marks for Student " << (i + 1) << " in Course " << (j + 1) << ": ";
       cin >> marks[i][j];
     }
+  }
+  
+  for (int i=0; i<3; i++){
+    int sum = 0;
+    for (int j=0; j<3; j++){
+      sum += marks[i][j];
+    }
+    cgpa[i] = sum / 3.0; 
+  }
+
+  for (int i=0; i<3; i++){
+    cout << "Student " << i+1 << " CGPA: " << cgpa[i] << endl;
   }
   
   return 0;
