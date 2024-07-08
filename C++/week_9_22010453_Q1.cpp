47dfcec577dd1a3a0e07d8d0f3ed8ffb30d06ced
Affan Najiy <affannajiy@gmail.com>
2024-07-08 15:14:17 +0800

    W9 Lab Revamp

--- C++/week_9_22010453_Q1.cpp
+++ C++/week_9_22010453_Q1.cpp
@@ -21,34 +21,36 @@ struct DateOfBirth{ //struct DOB
 struct Student{ //struct Student
   FullName name;
   DateOfBirth dob;
+  Course courses[10]; //Assumption 10 Courses per Students
   int n;
-  Course courses[n]; 
 };
 
 int main(){
   int num_students;
-  cout << "Enter number of students: ";
+  cout << "Enter Number of Students: ";
   cin >> num_students;
-
   Student students[num_students];
 
   for (int i = 0; i < num_students; ++i){ //Input
+    cout << "-------------------------- " << endl;
     cout << "Enter Student (" << (i + 1) << ") First Name: "; //First Name
     cin >> students[i].name.fname;
     cout << "Enter Student (" << (i + 1) << ") Last Name: "; //Last Name
     cin >> students[i].name.lname;
 
+    cout << "------------------------------------------------" << endl;
     cout << "Enter Student (" << (i + 1) << ") Day of Birth: "; //DayOB
     cin >> students[i].dob.day;
     cout << "Enter Student (" << (i + 1) << ") Month of Birth: "; //MonthOB
     cin >> students[i].dob.month;
     cout << "Enter Student (" << (i + 1) << ") Year of Birth: "; //YearOB
     cin >> students[i].dob.year;
-
-    cout << "Enter Number of Courses for Student " << (i + 1) << ": "; //Number of Courses
+    
+    cout << "------------------------------------------------" << endl;
+    cout << "Enter Number of Courses for Student " << (i + 1) << ": ";
     cin >> students[i].n;
 
-    for (int j = 0; j < students[i].n; ++j){
+    for (int j = 0; j < students[i].n; ++j){ //Courses
       cout << "Enter Course Code for Course " << (j + 1) << ": ";
       cin >> students[i].courses[j].course_code;
       cout << "Enter Course Name for Course " << (j + 1) << ": ";
@@ -62,15 +64,14 @@ int main(){
   cout << "----------------------\n";
 
   for (int i = 0; i < num_students; ++i){ //Output
-    cout << "+++STUDENT (" << (i + 1) << ")+++" << endl;
-    cout << "Full Name: " << students[i].name.fname << " " << students[i].name.lname << endl;
-    cout << "Date of Birth: " << students[i].dob.day << " " << students[i].dob.month << " " << students[i].dob.year << "\n";
+    cout << "\n+++STUDENT (" << (i + 1) << ")+++" << endl;
+    cout << "FULL NAME\t: " << students[i].name.fname << " " << students[i].name.lname << endl;
+    cout << "DATE OF BIRTH\t: " << students[i].dob.day << " " << students[i].dob.month << " " << students[i].dob.year << "\n";
 
-    cout << "Courses:\n";
+    cout << "COURSES:" << endl;
     for (int j = 0; j < students[i].n; ++j){
-      cout << "\tCourse " << (j + 1) << ": " << students[i].courses[j].course_code << " - " << students[i].courses[j].course_name << "\n";
+      cout << "\tCOURSE " << (j + 1) << ": " << students[i].courses[j].course_code << " - " << students[i].courses[j].course_name << "\n";
     }
-    cout << "\n";
   }
   return 0;
 }
