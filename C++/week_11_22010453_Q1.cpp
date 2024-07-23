75b9ae5172a1ee5b54a8bfe1b847f8605ca8a4c7
Affan Najiy <affannajiy@gmail.com>
2024-07-23 22:06:21 +0800

    CPP Function Revamp

--- C++/week_11_22010453_Q1.cpp
+++ C++/week_11_22010453_Q1.cpp
@@ -1,36 +1,50 @@
 #include <iostream>
+#include <iomanip> //For 2 Decimal Places(d.p)
 using namespace std;
 
-void calcArea(int l){
-  double area = l * l;
+void calcArea(double l, double w){ //Area Rectangle
+  double area = l * w;
   cout << "Area of Rectangle: " << area << endl;
 }
 
 void calcArea(double r){
   const double PI = 3.142;
   double area = PI * (r * r);
-  cout << "Area of Circle: " << area << endl;
+  cout << "Area of Circle   : " << area << endl;
 }
 
-void calcArea(int b, double h){
-  double area = (1 / 2) * b * h;
-  cout << "Area of Triangle: " << area << endl;
+void calcArea(double half, double b, double h){
+  double area = half * b * h;
+  cout << "Area of Triangle : " << area << endl;
 }
 
 int main(){
-  double length, radius, base, height;
+  double length, width, radius, base, height;
+
+  //Input
+  cout << "=======================" << endl;
+  cout << "SHAPES AREA CALCULATOR" << endl;
+  cout << "=======================" << endl;
+  cout << "++RECTANGLE Input++" << endl;
   cout << "Enter Length: ";
   cin >> length;
+  cout << "Enter Width: ";
+  cin >> width;
+  cout << "\n++CIRCLE Input++" << endl;
   cout << "Enter Radius: ";
   cin >> radius; 
+  cout << "\n++TRIANGLE Input++" << endl;
   cout << "Enter Base: ";
   cin >> base;
   cout << "Enter Height: ";
   cin >> height;
-
-  calcArea(length);
+  
+  //Output
+  cout << fixed << showpoint << setprecision(2); //Set for 2 d.p.
+  cout << "------------------" << endl;
+  calcArea(length, width);
   calcArea(radius);
-  calcArea(base, height);
-    
+  calcArea(0.5, base, height);
+  cout << "------------------" << endl;
   return 0;
 }
