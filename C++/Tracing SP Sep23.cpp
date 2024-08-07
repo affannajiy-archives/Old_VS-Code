29bd760254233fa38c33bc0ab915a442005f6324
Affan Najiy <affannajiy@gmail.com>
2024-08-07 17:31:45 +0800

    SP Final Exam Revision

---
+++ C++/Tracing SP Sep23.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+void pattern(int a, char b){
+  for(int j = 1; j <= a; j++){
+    cout << b << " ";
+    cout << endl;
+  }
+}
+
+int main(){
+  int line = 4;
+
+  for(int r = line; r >= 1; r--){
+    if (r % 2 == 0)
+    pattern(r, '$');
+    else
+    pattern(r, '@');
+  }
+  return 0;
+}
