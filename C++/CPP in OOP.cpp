7aa15fd1b499393dcc34be5c451ee1d6c73cd05d
Affan Najiy <affannajiy@gmail.com>
2024-09-11 09:14:23 +0800

    CPP in OOP Update

--- C++/CPP in OOP.cpp
+++ C++/CPP in OOP.cpp
@@ -2,50 +2,64 @@
 #include <iomanip>
 using namespace std;
 
-typedef struct{
+class Date{
+  public:
+  // data
   int day;
   int month;
   int year;
-} Date; // Only date
+}; // Only date
 
 class Transaction {
   public:
+  // data
   float val;
   Date date;
- 
-//2D Array
-  Transaction transactions[2][10];
+};
 
-  int option;
-
-  void InputFromConsole(int company){
+const int NUMTRANSACTIONS = 1000;
+class TransactionList {
+  public:
+  // data
+  Transaction transactions[10];  // the main data corresponds to the name of the class
+    
+  // operation
+  void InputFromConsole(){
     for (int i = 0; i < 10; i++){
       cout << "Enter the transaction value: ";
       cin >> transactions[i].val;
       cout << "Enter the transaction date (Day)/(Month)/(Year): ";
       cin >> transactions[i].date.day >> transactions[i].date.month >> transactions[i].date.year;
+      }
     }
-  }
-
-  void OutputToConsole(int company){
+  void OutputToConsole(){
     for (int i = 0; i < 10; i++){
       cout << "\nThe transaction value are RM " << fixed << setprecision(2) << transactions[i].val << endl;
-      cout << "Date: " << transactions[i].date.day << " / " << transactions[i].date.month <<  " / " << transactions[i].date.year;
+      cout << "Date: " << transactions[i].date.day << " / " << transactions[i].date.month << " / " << transactions[i].date.year;
+      }
     }
-  }
 };
 
+    // 2 companies, each company has 10 transactions
+    
+const int NUMCOMPANIES = 2;
+TransactionList transactions[NUMCOMPANIES]; // array of Transaction objects
+
+int option;
 
 int main() {
-  while (true){
-    cout << "Enter 1 for Input / 2 for Output: ";
-    cin >> option;
-    if (option == 1)
-      InputFromConsole();
-    else if (option == 2)
-      OutputToConsole();
-    else
-      cout << "Error" << endl;
+  for (int i=0; i<2; i++){ //2 Companies
+    while (true){
+      cout << "Enter 1 for Input / 2 for Output: ";
+      cin >> option;
+      if (option == 1)
+      // a single TransactionList object
+        transactions[i].InputFromConsole();
+      else if (option == 2)
+        transactions[i].OutputToConsole();
+      else
+        cout << "Error" << endl;
+      }
     }
   return 0;
 }
