da40f701a09ad1f312c7a010d14974d04e6004f8
Affan Najiy <affannajiy@gmail.com>
2024-09-27 10:58:33 +0800

    OOP Week 4 Lab

---
+++ C#/OOP Week4 Lab VSCode/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Ahoy There, What is your name?");
+string username = Console.ReadLine();
+Console.WriteLine("\nHello " + username + "!");
+Console.WriteLine("Pirate Status: Active");
+
+//
+if (username == "Iman Hazim" )
+{
+    Console.WriteLine("You are Muse!");
+    Console.WriteLine("Boom Shakalaka!");
+    Console.WriteLine("Go get Oil.")
+}
