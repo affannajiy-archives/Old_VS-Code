8847ce09ec3b6394783a2e2803a72365683609a2
Affan Najiy <affannajiy@gmail.com>
2024-09-28 13:41:12 +0800

    C# Prob

--- C#/MyFirstProgram/obj/Debug/net8.0/MyFirstProgram.AssemblyInfo.cs
+++ C#/MyFirstProgram/obj/Debug/net8.0/MyFirstProgram.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyFirstProgram")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dfd5f1732db068fc49f287dc8030b032f2dc5af5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+63fd052e657bae9d95894f135473bbf472076c9b")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyFirstProgram")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyFirstProgram")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
