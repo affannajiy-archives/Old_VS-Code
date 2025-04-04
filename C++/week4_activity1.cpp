5663175c2ef4f0525ff22a0802688c5993b355aa
Affan Najiy <affannajiy@gmail.com>
2025-04-05 03:38:54 +0800

    Removed CPP Presence
    Full blown extermination of CPP from VS Code

--- C++/week4_activity1.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-int main()
-{
-  cout << "Hello World!\t" << "Hello World!" << endl
-   << "Hello World!" << endl
-   << "Hello World!";
-  return 0;
-}
--- C++/week8_activity2.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main(){
-  int numbers[0];
-
-  return 0;
-}
